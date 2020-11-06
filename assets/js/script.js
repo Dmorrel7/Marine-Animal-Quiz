9daa7efa39aa7c2b2e4e2ac465fea0b582e8508a
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-11-06 17:12:53 -0500

    added variable for questions

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var quizQuestions = 
+[
+    {
+        question: "What is the fastest animal in the sea?",
+        a: "Mako Shark",
+        b: "Bluefin Tuna",
+        c: "Sailfish",
+        d: "Marlin",
+        answer: "c"
+    },
+
+    {
+        question: "Which animal is more venomous than the others?",
+        a:"Australian Box Jellyfish",
+        b:"Pufferfish",
+        c:"Stingray",
+        d:"Blue-Ringed Octopus",
+        answer:"a"
+    },
+
+    {
+        question:"What can a Blue Whale's weight get to?",
+        a:"155 tonnes",
+        b:"98 tonnes",
+        c:"208 tonnes",
+        d:"267 tonnes",
+        answer:"c"
+    },
+
+    {
+        question:"How long is a Lion's Mane Jellyfish?",
+        a:"47 ft.",
+        b:"108 ft.",
+        c:"132 ft",
+        d:"88 ft",
+        answer:"b"
+    },
+
+    {
+        question:"On average, how many teeth does a Bull Shark have at one time?",
+        a:"400",
+        b:"267",
+        c:"301",
+        d:"350",
+        answer:"d"
+    }
+];
